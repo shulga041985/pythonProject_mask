8fc3a96a70b62ff7f5a64ea49682c14a69383597
Sergey <king_artur2005@mail.ru>
2026-03-06 00:45:29 +0700

    Добавлена вторая функция выпилины баги в первой

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
-card_number = "счет 1254325698567854о"  # номера карты
-
-masked_card = mask_account_card(card_number)
+card_details = "счет 1254325698567854"  # номера карты или счета
+masked_card = mask_account_card(card_details)
 
 print(masked_card)
+
+input_date = "2024-03-11T02:26:18.671407"  # входной формат даты
+correct_date = get_date(input_date)
+
+print(correct_date)
--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,26 @@
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def mask_account_card(card_number: str) -> str:
+def mask_account_card(card_details: str) -> str:
     """Принимает  номер счёта или карты, маскировка в зависимости от данных"""
-
-    part_name = card_number.split()
-    if part_name[-1].isdigit():
-        number = int(part_name[-1])
+    part_name = card_details.split()  # разбиваем по пробелу
+    if part_name[-1].isdigit():  # забираем -1 индекс и проверяем что только цифры
+        number = int(part_name[-1])  # оборачиваем в целые числа
     else:
         return "Номер не состоит из цифр, введите корректный номер"
-    initials = " ".join(part_name[:-1])
-    if initials.upper() == "СЧЕТ":
+    initials = " ".join(part_name[:-1])  # делаем срез до - 1 индекса
+    if initials.upper() == "СЧЕТ":  # проверка на вхождение
         masks_number = get_mask_account(number)
     else:
         masks_number = get_mask_card_number(number)
     return f"{initials} {masks_number}"
+
+
+def get_date(input_date: str) -> str:
+    """
+    принимаем на вход строку содержащую дату и выводит в нужный формат
+    """
+    date = input_date[0:10]  # делаем срез
+    date_replace = date.split("-")  # разбиваем по минусу
+    date_revers = ".".join(date_replace[::-1])  # делаем реверс ставим точки
+    return date_revers
