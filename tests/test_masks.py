327bcc24e46c6253d455d7432d58e28294457500
Sergey <king_artur2005@mail.ru>
2026-03-29 20:25:03 +0700

    завершена работа с тестами

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,7 +4,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 # Для тестирования функции маскировки номера карты
 # Базовые тесты для функции
-def test_get_mask_card_number():
+def test_get_mask_card_number() -> None:
     assert get_mask_card_number("1234563212455632") == "1234 56** **** 5632"
     assert get_mask_card_number("1234-5632-1245-5632") == "1234 56** **** 5632"
     assert get_mask_card_number("1234-5632у1245у5632") == "Не корректный ввод номера карты."
@@ -13,21 +13,8 @@ def test_get_mask_card_number():
     assert get_mask_card_number("") == "Не корректный ввод номера карты."
 
 
-# Фикстура для создания тестовых данных
-@pytest.fixture
-def card_data():
-    return [
-        ("1234563212455632", "1234 56** **** 5632"),
-        ("1234-5632-1245-5632", "1234 56** **** 5632"),
-        ("1234-5632у1245у5632", "Не корректный ввод номера карты."),
-        ("1234 5632 1245 5632", "1234 56** **** 5632"),
-        ("123456321245563221321323", "Не корректный ввод номера карты."),
-        ("", "Не корректный ввод номера карты."),
-    ]
-
-
 # Проверка с фикстурой
-def test_fixture_card_number(card_data):
+def test_fixture_card_number(card_data: list[tuple]) -> None:
     for card_number, expected in card_data:
         result = get_mask_card_number(card_number)
         assert result == expected
@@ -45,14 +32,14 @@ def test_fixture_card_number(card_data):
         ("", "Не корректный ввод номера карты."),
     ],
 )
-def test_parametrize_card_number(card_number, expected):
+def test_parametrize_card_number(card_number: str, expected: str) -> None:
     result = get_mask_card_number(card_number)
     assert result == expected
 
 
 # Для тестирования функции маскировки номера счета
 # Базовые тесты для функции
-def test_get_mask_account():
+def test_get_mask_account() -> None:
     assert get_mask_account("1234563212455632") == "**5632"
     assert get_mask_account("1234-5632-1245-5632") == "**5632"
     assert get_mask_account("1234-5632у1245у5632") == "Не корректный ввод номера счета."
@@ -60,20 +47,9 @@ def test_get_mask_account():
     assert get_mask_account("") == "Не корректный ввод номера счета."
 
 
-# Фикстура для создания тестовых данных
-@pytest.fixture
-def account_number():
-    return [
-        ("1234563212455632", "**5632"),
-        ("1234-5632-1245-5632", "**5632"),
-        ("1234-5632у1245у5632", "Не корректный ввод номера счета."),
-        ("1234 5632 1245 5632", "**5632"),
-        ("", "Не корректный ввод номера счета."),
-    ]
-
 
 # Проверка с фикстурой
-def test_fixture_account_number(account_number):
+def test_fixture_account_number(account_number: list[tuple]) -> None:
     for acc_number, expected in account_number:
         result = get_mask_account(acc_number)
         assert result == expected
@@ -90,6 +66,6 @@ def test_fixture_account_number(account_number):
         ("", "Не корректный ввод номера счета."),
     ],
 )
-def test_parametrize_account_number(account_number, expected):
+def test_parametrize_account_number(account_number: str, expected: str) -> None:
     result = get_mask_account(account_number)
     assert result == expected
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -4,7 +4,7 @@ from src.widget import mask_account_card, get_date
 
 # Для тестирования функции маскировки номера карты
 # Базовые тесты для функции
-def test_mask_account_card():
+def test_mask_account_card() -> None:
     assert mask_account_card("счет 12345678912345678912") == "счет **8912"
     assert mask_account_card("VISA1254325698567854") == "Некорректный формат данных, проверьте ввод."
     assert mask_account_card("12543 25698567854") == "Некорректный формат данных, проверьте ввод."
@@ -13,21 +13,8 @@ def test_mask_account_card():
     assert mask_account_card("") == "Некорректный формат данных, проверьте ввод."
 
 
-# Фикстура для создания тестовых данных
-@pytest.fixture
-def card():
-    return [
-        ("счет 12345678912345678912", "счет **8912"),
-        ("VISA1254325698567854", "Некорректный формат данных, проверьте ввод."),
-        ("12543 25698567854", "Некорректный формат данных, проверьте ввод."),
-        ("VISA 1254325698567854", "VISA 1254 32** **** 7854"),
-        ("123456321245563221321323", "Некорректный формат данных, проверьте ввод."),
-        ("", "Некорректный формат данных, проверьте ввод."),
-    ]
-
-
 # Проверка с фикстурой
-def test_fixture_account_card(card):
+def test_fixture_account_card(card: list[tuple]) -> None:
     for card_details, expected in card:
         result = mask_account_card(card_details)
         assert result == expected
@@ -45,44 +32,39 @@ def test_fixture_account_card(card):
         ("", "Некорректный формат данных, проверьте ввод."),
     ],
 )
-def test_parametrize_account_card(card_details, expected):
+def test_parametrize_account_card(card_details: str, expected: str)-> None:
     result = mask_account_card(card_details)
     assert result == expected
 
 
- # Для тестирования функции маскировки номера счета
- # Базовые тесты для функции
-def test_get_date():
-    assert get_date("2024-03-11T02:26:18.671407") == '11.03.2024'
-    assert get_date('-2024-03-11T02:26:18.671407') == 'Ошибка ввода данных'
-    assert get_date('20-03-11T02:26:18.671407') == 'Ошибка ввода данных'
-    assert get_date('2024- -11T02:26:18.671407') == 'Ошибка ввода данных'
-    assert get_date('') == "Ошибка ввода данных"
+# Для тестирования функции маскировки номера счета
+# Базовые тесты для функции
+def test_get_date() -> None:
+    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
+    assert get_date("-2024-03-11T02:26:18.671407") == "Ошибка ввода данных"
+    assert get_date("20-03-11T02:26:18.671407") == "Ошибка ввода данных"
+    assert get_date("2024- -11T02:26:18.671407") == "Ошибка ввода данных"
+    assert get_date("") == "Ошибка ввода данных"
 
-# Фикстура для создания тестовых данных
-@pytest.fixture
-def g_date():
-    return [
-        ("2024-03-11T02:26:18.671407", '11.03.2024'),
-        ('-2024-03-11T02:26:18.671407', 'Ошибка ввода данных'),
-        ('20-03-11T02:26:18.671407', 'Ошибка ввода данных'),
-        ('2024- -11T02:26:18.671407', 'Ошибка ввода данных'),
-        ('', 'Ошибка ввода данных')
-        ]
 
 # Проверка с фикстурой
-def test_get_date(g_date):
+def test_get(g_date: list[tuple]) -> None:
     for date, expected in g_date:
         result = get_date(date)
         assert result == expected
 
+
 # Проверка  с параметризацией
-@pytest.mark.parametrize("input_date, expected", [
-    ("2024-03-11T02:26:18.671407", '11.03.2024'),
-    ('-2024-03-11T02:26:18.671407', 'Ошибка ввода данных'),
-    ('20-03-11T02:26:18.671407', 'Ошибка ввода данных'),
-    ('2024- -11T02:26:18.671407', 'Ошибка ввода данных'),
-    ('', 'Ошибка ввода данных')])
-def test_parametrize_get_date(input_date, expected):
+@pytest.mark.parametrize(
+    "input_date, expected",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("-2024-03-11T02:26:18.671407", "Ошибка ввода данных"),
+        ("20-03-11T02:26:18.671407", "Ошибка ввода данных"),
+        ("2024- -11T02:26:18.671407", "Ошибка ввода данных"),
+        ("", "Ошибка ввода данных"),
+    ],
+)
+def test_parametrize_get_date(input_date: str, expected: str) -> None:
     result = get_date(input_date)
     assert result == expected
