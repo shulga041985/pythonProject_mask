156a0ed6f8e7fac8576fe8ac98b49499e7a81d2e
Sergey <king_artur2005@mail.ru>
2026-04-16 11:40:30 +0700

    исправлено, проверено по Flake8

--- src/decorators.py
+++ src/decorators.py
@@ -12,15 +12,9 @@ def log(filename: Optional[str] = None) -> Callable:
                 result = func(*args, **kwargs)
                 if filename:
                     with open(filename, "a") as f:
-                        f.write(
-                            f"Function {func.__name__} status: Ok\n"
-                            f"Result: {result}\n"
-                        )
+                        f.write(f"Function {func.__name__} status: Ok\n" f"Result: {result}\n")
                 else:
-                    print(
-                        f"Function {func.__name__} status: Ok\n"
-                        f"Result: {result}\n"
-                    )
+                    print(f"Function {func.__name__} status: Ok\n" f"Result: {result}\n")
             except Exception as e:
                 print(f"Function {func.__name__} error: {type(e).__name__}: {e} (args={args}, kwargs={kwargs})\n")
                 raise
