0c18c52b604d3b00c221d05b71cabb8086d53478
Sergey <king_artur2005@mail.ru>
2026-04-16 11:36:10 +0700

    исправлено по замечаниям функции

--- src/decorators.py
+++ src/decorators.py
@@ -1,33 +1,25 @@
-from datetime import datetime
 from functools import wraps
-from typing import Any, Callable, Optional
+from typing import Optional, Callable, Any
 
 
 def log(filename: Optional[str] = None) -> Callable:
-    """Декоратор для фиксации запуска и выполнении функции"""
+    """Декоратор для фиксации запуска и выполнении функции без времени выполнения"""
 
     def decorator(func: Any) -> Callable:
         @wraps(func)
         def wrappers(*args: Any, **kwargs: Any) -> Any:
-
             try:
-                start_time = datetime.now()
                 result = func(*args, **kwargs)
-                stop_time = datetime.now()
                 if filename:
                     with open(filename, "a") as f:
                         f.write(
                             f"Function {func.__name__} status: Ok\n"
                             f"Result: {result}\n"
-                            f"Start time:{start_time}\n"
-                            f"Stop time:{stop_time}\n"
                         )
                 else:
                     print(
                         f"Function {func.__name__} status: Ok\n"
                         f"Result: {result}\n"
-                        f"Start time:{start_time}\n"
-                        f"Stop time:{stop_time}\n"
                     )
             except Exception as e:
                 print(f"Function {func.__name__} error: {type(e).__name__}: {e} (args={args}, kwargs={kwargs})\n")
